9a50edae154ffc1c9fd871c49e85151721236847
Himanshu Soni <sonihimanshu060@gmail.com>
2023-06-18 11:27:21 +0530

    Swap alternative element in the array

---
+++ swapAlternate.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Enter the size of the array: ";
+	cin >> n;
+	int arr[n];
+	cout << "Enter the array elements:" << endl;
+	for(int i=0;i<n;++i) {
+		cin >> arr[i];
+	}
+	for(int i=0;i<n;++i) {
+		int temp;
+		if(i%2 != 0) {
+			temp = arr[i];
+			arr[i] = arr[i+1];
+			arr[i+1] = temp;
+		}
+	}
+	cout << "The new array is: " << endl;
+	for(int i=0;i<n;++i) {
+		cout << arr[i] << " ";
+	}
+	return 0;
+}
